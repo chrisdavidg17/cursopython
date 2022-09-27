2afc12a31dc0cb8dab36e93c72749851d566d8f4
chrisdavidg17 <chrisdavidg17@gmail.com>
2022-09-26 21:54:27 -0500

    V1 import

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import operacioness
+import pprint
+
+def main():
+
+    pprint (dir(operacioness))
+    #sum = operacioness.suma (1200, 1600)    
+    #print(sum)
+
+if __name__ == '__main__':
+    main()
+
+   
+    
